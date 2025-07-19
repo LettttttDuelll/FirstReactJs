f59261c84888b92de8c0144e31460006fae283ac
LettttttDuelll <phamdung07102004@gmail.com>
2025-07-19 09:17:24 +0700

    Fix audio path for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './calculator.css';
+export default function MyApp() {
 
-function App() {
+  const [count, setCount] = useState("");
+
+  let audiom ;
+
+  function ClickOnce(MyButton) {
+    return (
+      setCount(count + MyButton.name)
+    );
+  }
+
+  function DeleteLast() {
+    if (count.length > 0) {
+      setCount(count.slice(0, -1));
+    }
+  }
+
+  function DeleteAll() {
+    return (
+      setCount(" ")
+    );
+  }
+
+  function PlayHope() {
+    if (audiom) {
+    audiom.pause();
+    audiom.currentTime = 0;
+  }
+    audiom = new Audio(`${process.env.PUBLIC_URL}/Hope.mp3`);
+    audiom.volume = 0.5; // Set volume to 50%
+    audiom.loop = true; // Loop the audio
+    audiom.playbackRate = 1.0; // Set playback speed to normal
+    audiom.play();
+  }
+
+  function PlayWorld() {
+    if (audiom) {
+    audiom.pause();
+    audiom.currentTime = 0;
+  }
+    audiom = new Audio(`${process.env.PUBLIC_URL}/World.mp3`);
+    audiom.volume = 0.5; // Set volume to 50%
+    audiom.loop = true; // Loop the audio
+    audiom.playbackRate = 1.0; // Set playback speed to normal
+    audiom.play();
+  }
+
+  function Equals() {
+    try {
+      // eslint-disable-next-line
+      const result = eval(count.replace(/x/g, '*')).toFixed(9);
+      setCount(result.toString());
+      return result;
+    } catch (error) {
+      console.error("Invalid expression", error);
+      setCount("Error");
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Hand-up Calculator</h1>
+        <div className='ShowResult'>
+          <h1>{count}</h1>
+        </div>
+      </div>
+      <div>
+        <button className="MyButton" onClick={PlayHope} >Hope</button>
+        <button className="MyButton" onClick={PlayWorld}>World</button>
+        <MyButton onClick={DeleteLast} name={'AC'} />
+        <MyButton onClick={DeleteAll} name={'DEL'} />
+      </div>
+      <div>
+        <MyButton onClick={ClickOnce} name={'1'} />
+        <MyButton onClick={ClickOnce} name={'2'} />
+        <MyButton onClick={ClickOnce} name={'3'} />
+        <MyButton onClick={ClickOnce} name={'+'} />
+      </div>
+      <div>
+        <MyButton onClick={ClickOnce} name={'4'} />
+        <MyButton onClick={ClickOnce} name={'5'} />
+        <MyButton onClick={ClickOnce} name={'6'} />
+        <MyButton onClick={ClickOnce} name={'/'} />
+      </div>
+      <div>
+        <MyButton onClick={ClickOnce} name={'7'} />
+        <MyButton onClick={ClickOnce} name={'8'} />
+        <MyButton onClick={ClickOnce} name={'9'} />
+        <MyButton onClick={ClickOnce} name={'x'} />
+      </div>
+      <div>
+        <MyButton onClick={ClickOnce} name={'.'} />
+        <MyButton onClick={ClickOnce} name={'0'} />
+        <MyButton onClick={Equals} name={'='} />
+        <MyButton onClick={ClickOnce} name={'/'} />
+      </div>
+    </>
   );
 }
 
-export default App;
+function MyButton({ name, onClick }) {
+  return (
+    <button className="MyButton" onClick={() => onClick({ name })}
+    >{name}</button>
+  );
+}
